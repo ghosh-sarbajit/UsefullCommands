31365dcfebd1c666b9710d88d627d5597ecff3fe
Sarbajit Ghosh <37039890+ghosh-sarbajit@users.noreply.github.com>
2025-07-02 10:23:00 +0530

    Update and rename AfterNewInstallation.md to AfterNewInstallation.sh

--- AfterNewInstallation.sh
+++ AfterNewInstallation.sh
@@ -1,3 +1,4 @@
+### for VM
 ### Install Guest Additions CD Image on VirtualBox
 
 sudo apt-get purge libreoffice*
@@ -10,10 +11,23 @@ dpkg -l | grep libreoffice
 
 sudo apt-get update
 
+sudo apt-get install build-essential gcc make perl dkms
+
+# for real machine
+
 sudo apt-get install build-essential gcc make perl dkms python3-pip vim net-tools git tmux screen openssh-server ffmpeg
 
 sudo apt-get install gedit gimp shotwell dia xfig obs-studio xournalpp vlc
 
 reboot
 
-other things to install: R, RStudio, Miniconda, VSConde, VirtualBox
+other things to install: R, RStudio, Miniconda, VSCode, VirtualBox
+
+# some important files
+
+ssh client
+ssh localhost
+/etc/ssh/sshd_config
+
+sudo vim /etc/default/grub
+sudo update-grub
