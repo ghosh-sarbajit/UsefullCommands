936074793c1e97bd114b5750dbeace7f6073d0f8
Sarbajit Ghosh <37039890+ghosh-sarbajit@users.noreply.github.com>
2025-07-05 20:46:09 +0530

    Update bashrc_mods.sh

--- bashrc_mods.sh
+++ bashrc_mods.sh
@@ -1,9 +1,14 @@
 # dir aliases
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
+alias up1='cd ../'
+alias up2='cd ../../'
+alias up3='cd ../../../'
+alias up4='cd ../../../../'
+alias up5='cd ../../../../../'
+alias up6='cd ../../../../../../'
+alias up7='cd ../../../../../../../'
+alias up8='cd ../../../../../../../../'
+alias up9='cd ../../../../../../../../../'
+alias up10='cd ../../../../../../../../../../'
 
 up () {
     local d=""
